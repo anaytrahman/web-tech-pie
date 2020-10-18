05247ad3f254694e2890172d0647910b0ef05b1c
Anayt Rahman <anaytrahman786@gmail.com>
2020-10-18 13:10:45 +0530

    added new code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionOneComponent } from './section-one/section-one.component';
+import { SectionTwoComponent } from './section-two/section-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SectionOneComponent
+    SectionOneComponent,
+    SectionTwoComponent
   ],
   imports: [
     BrowserModule,
